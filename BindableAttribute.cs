b98e356bbdffa3fc1874b01e36d39c8f26d47918
JesOb <JesObertyukh@gmail.com>
2024-01-15 04:49:00 +0200

    + bindable and callable core concepts

---
+++ BindableAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Flexy.Core
+{
+	[AttributeUsage( AttributeTargets.Property | AttributeTargets.Method )]
+	public class BindableAttribute: UnityEngine.Scripting.PreserveAttribute
+	{
+		public	String		Description;
+		public	Boolean		IsWarning;
+	}
+}
---
+++ CallableAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace Flexy.Core
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public class CallableAttribute : UnityEngine.Scripting.PreserveAttribute
+	{
+		public		CallableAttribute			( ) { }
+		public		CallableAttribute			( Type argumentType ) { ArgumentType = argumentType; }
+		
+		public		Type	ArgumentType {get; init;}
+	}
+}
