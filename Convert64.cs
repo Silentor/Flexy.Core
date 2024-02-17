72f297e645f9df667a20a425f5754b648336e3d3
JesOb <JesObertyukh@gmail.com>
2024-02-17 12:57:35 +0200

    + convert 64

---
+++ Convert64.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Collections.LowLevel.Unsafe;
+
+namespace Flexy.Core;
+
+public static class Convert64
+{
+	public static T2	As<T1, T2>( T1 data )  => new Convert64Memory<T1>{ Data = data }.As<T2>( );
+}
+
+internal struct Convert64Memory<T1> 
+{
+	public	T1		Data;
+	public	UInt64	_padding;
+		
+	public	T2		As<T2>() => UnsafeUtility.As<T1, T2>( ref Data ); 
+}
