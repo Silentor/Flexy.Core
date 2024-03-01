c6aa73b28d2534b9b7484c177b5cb4f560e58164
JesOb <JesObertyukh@gmail.com>
2024-03-01 19:19:01 +0200

    + open project button

---
+++ Editor/OpenProjectFolderButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Flexy.Core.Editor;
+
+[InitializeOnLoad]
+public static class OpenProjectFolderButton
+{
+	static OpenProjectFolderButton( ) { UnityEditorTopToolbar.AddIMGUIContainerToRightPocket( OnTestRunGUI ); }
+	
+	private static void		OnTestRunGUI			( )	
+	{
+		GUILayout.FlexibleSpace( );
+		if( GUILayout.Button( "Open Project Folder", EditorStyles.toolbarButton, GUILayout.Height(14) ) )
+		{
+			Application.OpenURL( Application.dataPath );
+		}
+	}
+}
