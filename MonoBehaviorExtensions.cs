5f97b3e7c0dd474ef1970e890801551efa841380
JesOb <JesObertyukh@gmail.com>
2024-04-09 15:16:26 +0300

    + monoBehaviorExtensions

--- MonoBehaviorExtensions.cs
+++ MonoBehaviorExtensions.cs
@@ -2,5 +2,7 @@
 
 public static class MonoBehaviorExtensions
 {
-	public static T 	OrNull<T>( this T obj ) where T: UnityEngine.Object => (obj) ? obj : null;
+	public static		T 			OrNull<T>	( this T obj ) where T: UnityEngine.Object => (obj) ? obj : null;
+	public static		Boolean		IsAlive		( this UnityEngine.Object obj ) => obj;
+	public static		void		Destroy		( this UnityEngine.Object obj ) => UnityEngine.Object.Destroy( obj );
 }
