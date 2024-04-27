f7f8828450ce7e5ebf2308f322658ef5198df555
JesOb <JesObertyukh@gmail.com>
2024-04-27 10:33:05 +0300

    + Single Extensions

---
+++ SingleExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Flexy.Core;
+
+public static class SingleExtensions
+{
+	public const Single ZeroTolerance = 1e-5f;
+	
+	public static Boolean	Near		( this Single value0, Single value1, Single epsilon = ZeroTolerance )
+	{
+		return Math.Abs( value0 - value1 ) <= epsilon;
+	}
+}
